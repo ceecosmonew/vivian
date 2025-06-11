6692809129edf7dd515940071ab5629d35b220e5
ceecosmonew <pagefamex@gmail.com>
2025-06-11 22:19:58 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,7 +31,7 @@ function Footer() {
           <a href="/pdf/vivian.pdf" className="text-white" download>Download CV</a> |
           <a href="https://linkedin.com/in/ur profile" target="_blank" rel="noreferrer" className="text-white mx-2"><i className='fa-brands fa-linkedin-in fa-2x text-primary'></i></a>
           <a href="https://twitter.com/" target="_blank" rel="noreferrer" className="text-white mx-2"><i className='fa-brands fa-twitter fa-2x text-info'></i></a>
-          <a href="https://facebook.com/vijos enterprises" target="_blank" rel="noreferrer" className="text-white mx-2"><i className='fa-brands fa-facebook fa-2x text-info'></i></a>
+          <a href="https://facebook.com/Vijos Enterprises" target="_blank" rel="noreferrer" className="text-white mx-2"><i className='fa-brands fa-facebook fa-2x text-info'></i></a>
           <a href="https://wa.me/2348064560834" target="_blank" rel="noreferrer" className="text-white mx-2"><i className='fa-brands fa-whatsapp fa-2x text-info'></i></a>
 
 

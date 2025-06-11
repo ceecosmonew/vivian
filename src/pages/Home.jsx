17e1610f7b04d36c7a7b5a625577ae38e2063d2f
ceecosmonew <pagefamex@gmail.com>
2025-06-11 21:53:24 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,15 +29,17 @@ import AnimatedSection from '../components/AnimatedSection';
 function Home() {
   return (
     <AnimatedSection>
-      <Container className="text-center py-1 text-primary">
-         <h5 className=" display-7 fw-bold text-primary animate__animated animate__fadeIn">Welcome to My Portfolio</h5>
+      <Container className="text-center py-3 text-primary">
+         <h5 className=" display-6 fw-bold text-primary animate__animated animate__fadeIn">Welcome to My Portfolio</h5>
         {/* <h3 className="display-4 animate__animated animate__fadeIn">Hi, I'm a Frontend Developer</h3> */}
-        <p className="leads"><h6>I'm a passionate beginner Frontend Developer I enjoy turning ideas into interactive, user-friendly web experiences. I'm currently growing my skills and learning real-world development practices.</h6>
+        <p className="leads"><h5>I'm a passionate beginner Frontend Developer I enjoy turning ideas into interactive, 
+          user-friendly web experiences. I'm currently growing my skills and learning real-world development practices.</h5>
 
           <br />
-          <h6>I specialize in HTML, CSS, JavaScript, and React.js, and I’m constantly learning to improve my skills.</h6>
+          <h5>I specialize in HTML, CSS, JavaScript, and React.js, and I’m constantly learning to improve my skills.</h5>
         </p>
-        <p><h6>I have strong online communication skills and a background in marketing, which help me work efficiently with remote teams and understand user needs.</h6></p>
+        <p><h5>I have strong online communication skills and a background in marketing, which
+          help me work efficiently with remote teams and understand user needs.</h5></p>
         <Button href="/pdf/vivian.pdf" className="mt-3" variant="outline-primary">Download My CV</Button>
 
       </Container>
